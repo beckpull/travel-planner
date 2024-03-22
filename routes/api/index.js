b853f11ff70090b746641e1f14ed61d3da509d85
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-03-21 20:55:47 -0600

    got backend structure almost ready, just need classes and routes defined now

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const locationRoutes = require('./locationRoutes');
+const travelerRoutes = require('./travelerRoutes');
+const tripRoutes = require('./tripRoutes');
+
+router.use('/locations', locationRoutes);
+router.use('/travelers', travelerRoutes);
+router.use('/trips', tripRoutes);
